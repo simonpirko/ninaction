c1c0f72fd62fdb867d8651a74c951129f58412e0
simonpirko <s.pirko@faceclub.app>
2022-10-01 14:20:13 +0300

    Add fail test

--- src/test/java/com/example/ninaction/NinactionApplicationTests.java
+++ src/test/java/com/example/ninaction/NinactionApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.ninaction;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class NinactionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail();
 	}
 
 }
